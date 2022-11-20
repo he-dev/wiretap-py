2ca98f0ee781e677b997c5589ce94af93487569a
ᎬᏁᎶᎥᏁᎬᎬᏒ <bennyboss+github@gmail.com>
2022-11-20 13:02:05 +0100

    Refactor module & improve logging configuration

--- tests/demo.py
+++ tests/demo.py
@@ -1,37 +1,60 @@
 import logging
+import logging.config
 
-import wiretap.src.wiretap.wiretap as wiretap
-from wiretap.src.wiretap.wiretap import UnitOfWork, UnitOfWorkScope, telemetry
+import wiretap.src.wiretap as wiretap
+from wiretap.src.wiretap import UnitOfWork, UnitOfWorkScope, telemetry
 from wiretap_handlers_sqlite.src.wiretap.handlers.sqlite import SQLiteHandler
 from wiretap_handlers_sqlserver.src.wiretap.handlers.sqlserver import SqlServerHandler, SqlServerConnection
 
 
-def initialize_logger() -> logging.Logger:
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(logging.Formatter(style="{", fmt="{asctime} | {module}.{funcName} | {status} | {extra}", defaults={"status": "<status>", "extra": "<extra>"}))
-    # sql_server_handler = SqlServerHandler(
-    #    SqlServerConnection(server="localhost,1433", database="master", username="sa", password="blub123!"),
-    #    "INSERT INTO log([timestamp], [scope], [status], [extra], [comment]) VALUES (?, ?, ?, ?, ?)"
-    # )
-
-    # handlers = [stream_handler, sql_server_handler]
-    handlers = [stream_handler]
-
-    logging.root.addHandler(stream_handler)
-
-    logger = logging.Logger(UnitOfWork.__name__)
-    for h in handlers:
-        logger.addHandler(h)
-
-    return logger
+# sql_server_handler = SqlServerHandler(
+#    SqlServerConnection(server="localhost,1433", database="master", username="sa", password="blub123!"),
+#    "INSERT INTO log([timestamp], [scope], [status], [extra], [comment]) VALUES (?, ?, ?, ?, ?)"
+# )
 
 
+def configure_logging():
+    logging.config.dictConfig({
+        "version": 1,
+        "formatters": {
+            "console": {
+                "style": "{",
+                "format": "{asctime}.{msecs:.0f} | {module}.{funcName} | {status} | {extra}",
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+                "defaults": {"status": "<status>", "extra": "<extra>"}
+            }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "console",
+                "level": "INFO"
+            },
+            "file": {
+                "class": "logging.handlers.TimedRotatingFileHandler",
+                "when": "d",
+                "interval": 1,
+                "filename": r"c:\temp\wiretap.log",
+                "formatter": "console",
+                "level": "INFO"
+            }
+        },
+        "loggers": {
+            "": {
+                "handlers": ["console", "file"],
+                "level": "INFO"
+            }
+        }
+    })
 
-# @telemetry(extra={"layer": "application"})
+
+configure_logging()
+
+
 @telemetry(**wiretap.APPLICATION)
 def flow_decorator_test(value: int, scope: UnitOfWorkScope = None):
     scope.running(foo=value)
-    raise ValueError
+    # raise ValueError
     pass
 
 
@@ -49,5 +72,5 @@ def flow_test():
 
 
 if __name__ == "__main__":
-    flow_test()
+    # flow_test()
     flow_decorator_test(7)
